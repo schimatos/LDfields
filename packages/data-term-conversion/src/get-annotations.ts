e63214a8c9b64f6a57fc1f492b0108c46478e5fa
jeswr <jesse.wright@anu.edu.au>
2021-02-17 09:19:08 +1100

    WIP

--- packages/data-term-conversion/src/get-annotations.ts
+++ packages/data-term-conversion/src/get-annotations.ts
@@ -1,22 +1,28 @@
 import { namedNode, literal, quad } from '@rdfjs/data-model';
 import type { NamedNode, BlankNode } from 'rdf-js';
 
-enum rdf {
+const enum rdf {
   term = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#term',
   type = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
 }
 
-enum rdfs {
+const enum rdfs {
   label = 'http://www.w3.org/2000/01/rdf-schema#label',
 }
 
 export function getAnnotations<
-  Props extends { [key: string]: string; }
->(metadata: Props, term: NamedNode | BlankNode) {
+  T extends string
+>(metadata: { [K in T]?: string }, term: NamedNode | BlankNode) {
   const annotations = [];
   if (rdf.type in metadata) {
     // TODO [Future]: Handle case where class is a blank node
-    annotations.push(quad(term, namedNode(rdf.type), namedNode(metadata[rdf.type])));
+    annotations.push(
+      quad(
+        term,
+        namedNode(rdf.type),
+        namedNode(metadata['http://www.w3.org/1999/02/22-rdf-syntax-ns#type']),
+      ),
+    );
   }
   if (rdfs.label in metadata) {
     annotations.push(
