51b9575a1a0592011ad13219f7264f489af65f50
jeswr <jesse.wright@anu.edu.au>
2021-04-04 20:42:02 +1000

    WIP

--- packages/fields/react/generic/react-generic-search/src/index.tsx
+++ packages/fields/react/generic/react-generic-search/src/index.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import { LDfieldGenericBase } from '@ldfields/generic-field-base';
-import type { FieldProps } from '@ldfields/field-base/types';
-import { useState } from '@jeswr/use-state';
-import type { IQueryEngine } from '@comunica/types';
-import Selector from 'sparql-search-bar';
+// import React, { useEffect } from 'react';
+// import { LDfieldGenericBase } from '@ldfields/generic-field-base';
+// import type { FieldProps } from '@ldfields/field-base/types';
+// import { useState } from '@jeswr/use-state';
+// import type { IQueryEngine } from '@comunica/types';
+// import Selector from 'sparql-search-bar';
 
 // function fieldFactory<
 //   Props extends { [key: string]: string | undefined; },
@@ -18,10 +18,10 @@ import Selector from 'sparql-search-bar';
 //       <>
 //         <label>{label ?? (modifier ? /[a-z]*$/i.exec(modifier)?.[0] : '')}</label>
 //         <Selector
-//           // TODO: Fix type casting here 
+//           // TODO: Fix type casting here
 //           queryEngine={data?.queryEngine as Promise<IQueryEngine>}
 //           onChange={onChange}
-          
+
 //         />
 //       </>
 //     );
