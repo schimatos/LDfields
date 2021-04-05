95630c7d5bfbc4ff7a5be85b07b1ba3466b55f11
jeswr <jesse.wright@anu.edu.au>
2021-04-05 23:16:05 +1000

    WIP

--- packages/fields/react/generic/react-generic-search/src/index.tsx
+++ packages/fields/react/generic/react-generic-search/src/index.tsx
@@ -1,47 +1,50 @@
-import React, { useEffect } from 'react';
-import { LDfieldGenericBase } from '@ldfields/generic-field-base';
-import type { FieldProps } from '@ldfields/field-base/types';
-import { useState } from '@jeswr/use-state';
-import type { IQueryEngine } from '@comunica/types';
-import Selector from 'sparql-search-bar';
+// import React, { useEffect } from 'react';
+// import { LDfieldGenericBase } from '@ldfields/generic-field-base';
+// import type { FieldProps } from '@ldfields/field-base/types';
+// import { useState } from '@jeswr/use-state';
+// import type { IQueryEngine } from '@comunica/types';
+// import Selector from 'sparql-search-bar';
 
-function fieldFactory<
-  Props extends { [key: string]: string | undefined; },
-  ExtraData extends { queryEngine: Promise<IQueryEngine>, pathFactory: any }
->(modifier: string) {
-  return function Field({ props, onChange, label, data }: FieldProps<Props, ExtraData>) {
-    const [value, setValue] = useState<string>(props[modifier] ?? '');
-    useEffect(() => {
-      setValue(props.value ?? '');
-    }, [props.value]);
-    return (
-      <>
-        <label>{label ?? (modifier ? /[a-z]*$/i.exec(modifier)?.[0] : '')}</label>
-        <Selector
-          // TODO: Fix type casting here
-          queryEngine={data?.queryEngine as Promise<IQueryEngine>}
-          pathFactory={data?.pathFactory}
-          // TODO: FIX TS-IGNORE
-          // @ts-ignore
-          onChange={e => { onChange(e as Partial<Props>) }}
+// TODO: Make this *not* generic
 
-        />
-      </>
-    );
-  };
-}
+// function fieldFactory<
+//   Props extends { [key: string]: string | undefined; },
+//   ExtraData extends { queryEngine: Promise<IQueryEngine>, pathFactory: any }
+// >(modifier: string) {
+//   return function Field({ props, onChange, label, data }: FieldProps<Props, ExtraData>) {
+//     const [value, setValue] = useState<string>(props[modifier] ?? '');
+//     useEffect(() => {
+//       setValue(props.value ?? '');
+//     }, [props.value]);
+//     return (
+//       <>
+//         <label>{label ?? (modifier ? /[a-z]*$/i.exec(modifier)?.[0] : '')}</label>
+//         <Selector
+//           // TODO: Fix type casting here
+//           queryEngine={data?.queryEngine as Promise<IQueryEngine>}
+//           pathFactory={data?.pathFactory}
+//           // TODO: FIX TS-IGNORE
+//           // @ts-ignore
+//           onChange={e => { onChange(e as Partial<Props>) }}
+//           data={props}
 
-export class SparqlSearch<
-  Props extends { [key: string]: string | undefined; },
-  ExtraData = never,
-> extends LDfieldGenericBase<JSX.Element, Props> {
-  priority = 55;
+//         />
+//       </>
+//     );
+//   };
+// }
 
-  supports() {
-    return true;
-  }
+// export class SparqlSearch<
+//   Props extends { [key: string]: string | undefined; },
+//   ExtraData = never,
+// > extends LDfieldGenericBase<JSX.Element, Props> {
+//   priority = 55;
 
-  Field = fieldFactory<Props>(this.modifier)
-}
+//   supports() {
+//     return true;
+//   }
 
-// export {};
+//   Field = fieldFactory<Props>(this.modifier)
+// }
+
+export {};
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import '../lib/styles.css'
@@ -5,21 +6,21 @@ import ReactDOM from 'react-dom';
 import Input from '@ldfields/default-react';
 import { useState } from '@jeswr/use-state';
 import { namedNode } from '@rdfjs/data-model';
-import { Data } from '@ldfields/props-rdf-conversion/types';
+import type { Data } from '@ldfields/props-rdf-conversion/types';
 
 function App() {
   const [props, setData] = useState<Data>({
     term: namedNode('http://example.org#Jesse'),
     annotations: [],
-  })
+  });
   return <Input
     props={props}
-    onChange={e => {
-      console.log('on change triggered with value', e)
+    onChange={(e) => {
+      console.log('on change triggered with value', e);
       setData(e);
     }}
     constraints={{}}
-  />
+  />;
 }
 
 ReactDOM.render(
